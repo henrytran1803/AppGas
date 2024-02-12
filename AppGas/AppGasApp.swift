361a7baadda92abe6cd3e2178bd277576fa781b0
Trần Việt Anh <tranvietanh1803@gmail.com>
2024-02-12 12:00:10 +0700

    Initial Commit

---
+++ AppGas/AppGasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppGasApp.swift
+//  AppGas
+//
+//  Created by Tran Viet Anh on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppGasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
